5b34587444cf1d0390751db2f58d9fcbd0faad26
Guilherme Selair <guilherme.lima1997@hotmail.com>
2021-06-19 18:05:34 -0300

    Update server.ts and Procfile2

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import 'reflect-metadata';
-import express, {  } from 'express';
+import express from 'express';
 import cors from 'cors';
 
 import routes from './routes';
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3333, () => console.log("[ON] Server running on port 3333"));
+app.listen(port, () => console.log("[ON] Server running on port 3333"));
