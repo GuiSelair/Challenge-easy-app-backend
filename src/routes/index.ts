c39e14487bb38a4129e8471f6043e6ec7f194bad
Guilherme Selair <guilherme.lima1997@hotmail.com>
2021-06-19 17:04:52 -0300

    Initial commit

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.get("/", (request, response) => {
+  return response.json({
+    ola: "world",
+  })
+})
+
+export default routes;
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import 'reflect-metadata';
+import express, {  } from 'express';
+import cors from 'cors';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => console.log("[ON] Server running on port 3333"));
