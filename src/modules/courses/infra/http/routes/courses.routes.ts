49728f5e7976b052d605eb6f40c184ce20dde858
Guilherme Selair <guilherme.lima1997@hotmail.com>
2021-06-22 01:34:15 -0300

    Feat: Added middleware

--- src/modules/courses/infra/http/routes/courses.routes.ts
+++ src/modules/courses/infra/http/routes/courses.routes.ts
@@ -3,19 +3,21 @@ import { Router } from 'express';
 import CoursesController from '../controllers/CoursesController';
 import LevelCoursesController from '../controllers/LevelCoursesController';
 
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensuredAuthenticated';
+
 const coursesRouter = Router();
 const coursesController = new CoursesController();
 const levelCoursesController = new LevelCoursesController();
 
 // NIVEL DO CURSO
-coursesRouter.post("/:id/level/create", levelCoursesController.create);
-coursesRouter.put("/:idLevel/level/update", levelCoursesController.update);
-coursesRouter.delete("/:idLevel/level/delete", levelCoursesController.delete);
-coursesRouter.get("/listAllLevels", levelCoursesController.show);
-coursesRouter.get("/listLevel/:id/", levelCoursesController.index);
+coursesRouter.post("/:id/level/create", ensureAuthenticated, levelCoursesController.create);
+coursesRouter.put("/:idLevel/level/update", ensureAuthenticated, levelCoursesController.update);
+coursesRouter.delete("/:idLevel/level/delete", ensureAuthenticated, levelCoursesController.delete);
+coursesRouter.get("/listAllLevels", ensureAuthenticated, levelCoursesController.show);
+coursesRouter.get("/listLevel/:id/", ensureAuthenticated, levelCoursesController.index);
 
 // CURSOS
 coursesRouter.get("/", coursesController.show);
-coursesRouter.get("/:id", coursesController.index);
+coursesRouter.get("/:id", ensureAuthenticated, coursesController.index);
 
 export default coursesRouter;
