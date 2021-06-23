22cfa9c4739b690b6692b83fc8e2798dc64fd37d
Guilherme Selair <guilherme.lima1997@hotmail.com>
2021-06-23 16:25:45 -0300

    Feat: Download PDf8

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -25,17 +25,16 @@ routes.post("/generatePDF", ensureAuthenticated ,(request: Request, response: Re
 
   pdf.text(JSON.stringify(body));
 
-  console.log(JSON.stringify(body));
-  
   // console.log(`${path.resolve(__dirname, "..", "..", "..", "..", "..", "reports")}/pdftest.pdf`, 'pdftest.pdf');
   
 
   pdf.pipe(fs.createWriteStream(`${path.resolve(__dirname, "..", "..", "..", "..", "..", "reports")}/pdftest.pdf`));
   pdf.end();
-  return response.sendFile(`${path.resolve(__dirname, "..", "..", "..", "..", "..", "reports")}/pdftest.pdf`);
-  // return response.json({
-  //   "url": `${path.resolve(__dirname, "..", "..", "..", "..", "..", "reports")}/pdftest.pdf`,
-  // });
+  // return response.sendFile(`${path.resolve(__dirname, "..", "..", "..", "..", "..", "reports")}/pdftest.pdf`);
+  return response.json({
+    "url": `${path.resolve(__dirname, "..", "..", "..", "..", "..", "reports")}/pdftest.pdf`,
+    "dadosSalvos": body,
+  });
 
 });
 
